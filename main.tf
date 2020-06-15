44c6b9dfd9fa8ea4449d2de53d5ecb89d7e68b04
chandanshukla1989 <50393216+chandanshukla1989@users.noreply.github.com>
2020-06-16 03:33:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,18 +24,7 @@ resource "aws_subnet" "public-subnet-3" {
 resource "aws_route_table" "public-route-table" {
   vpc_id = "${aws_vpc.production-vpc.id}"
 }
-resource "aws_route_table_association" "public-route-1-association" {
-  route_table_id = "${aws_route_table.public-route-table.id}"
-  subnet_id      = "${aws_subnet.public-subnet-1.id}"
-}
-resource "aws_route_table_association" "public-route-2-association" {
-  route_table_id = "${aws_route_table.public-route-table.id}"
-  subnet_id      = "${aws_subnet.public-subnet-2.id}"
-}
-resource "aws_route_table_association" "public-route-3-association" {
-  route_table_id = "${aws_route_table.public-route-table.id}"
-  subnet_id      = "${aws_subnet.public-subnet-3.id}"
-}
+
 resource "aws_subnet" "private-subnet-1" {
   cidr_block        = "${var.private_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
